1707f1ec23f33ee5e5b463a006f521d62f4801bf
Bilyana Yordanova <sarcoholic98@gmail.com>
2021-01-24 13:15:10 +0200

    Refactor js files

---
+++ static/canvas.js
@@ -0,0 +1,115 @@
+onMouseDown = function (canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    var x = event.clientX - rect.left;
+    var y = event.clientY - rect.top;
+
+    if (hScroll.mouseDown(x, y)) {
+    } else if (vScroll.mouseDown(x, y)) {
+    } else {
+        event.preventDefault();
+        col = x;
+        row = y;
+
+        col = Math.floor(col / cellWidth) - 1 + firstColumn;
+        row = Math.floor(row / cellHeight) - 1 + firstRow;
+
+        if (col < firstColumn) {
+            table.rowOnFocus = row;
+            table.colOnFocus = -1;
+            table.onFocus = table.getCell(row, 0);
+        }
+        else if (row < firstRow) {
+            table.colOnFocus = col;
+            table.rowOnFocus = -1;
+            table.onFocus = table.getCell(0, col);
+        }
+        else {
+            table.rowOnFocus = -1;
+            table.colOnFocus = -1;
+
+            table.onFocus = table.getCell(row, col);
+            cell = table.getCell(row, col);
+        }
+
+        document.getElementById("input-value").value = cell.text;
+        document.getElementById("input-value").focus();
+    }
+
+    table.update();
+}
+
+onMouseUp = function (event) {
+    var rect = canvas.getBoundingClientRect();
+    var x = event.clientX - rect.left;
+    var y = event.clientY - rect.top;
+
+    hScroll.mouseUp(x, y);
+    vScroll.mouseUp(x, y);
+    table.update();
+}
+
+onMouseMove = function (event) {
+    var rect = canvas.getBoundingClientRect();
+    var x = event.clientX - rect.left;
+    var y = event.clientY - rect.top;
+
+    oldFirstColumn = firstColumn;
+    oldFirstRow = firstRow;
+    hScroll.mouseMove(x, y);
+    vScroll.mouseMove(x, y);
+    if (oldFirstColumn != firstColumn || oldFirstRow != firstRow)
+        table.update();
+}
+
+canvas.onmousedown = function (event) {
+    onMouseDown(canvas, event);
+}
+
+input.addEventListener("keydown", function (event) {
+    if (event.key == "Enter") {
+        event.preventDefault();
+        setCell();
+
+        newRow = table.onFocus.row + 1;
+        table.rowOnFocus = -1;
+        table.colOnFocus = -1;
+
+        cell = table.getCell(newRow, table.onFocus.col);
+        table.onFocus = cell;
+
+        document.getElementById("input-value").value = table.onFocus.text;
+        document.getElementById("input-value").focus();
+        table.update();
+    }
+    if (event.key == "Tab") {
+        event.preventDefault();
+        setCell();
+
+        newCol = table.onFocus.col + 1;
+        table.rowOnFocus = -1;
+        table.colOnFocus = -1;
+
+        cell = table.getCell(table.onFocus.row, newCol);
+        table.onFocus = table.getCell(table.onFocus.row, 0);
+        table.onFocus = cell;
+
+        document.getElementById("input-value").value = table.onFocus.text;
+        document.getElementById("input-value").focus();
+        table.update();
+    }
+});
+
+input.addEventListener("input", function (event) {
+    setCell();
+});
+
+div.addEventListener("wheel", function (event) {
+    firstRow += event.deltaY;
+    firstColumn += event.deltaX;
+    firstRow = Math.max(0, firstRow);
+    firstColumn = Math.max(0, firstColumn);
+
+    hScroll.autoSize();
+    vScroll.autoSize();
+    table.update();
+});
---
+++ static/globals.js
@@ -0,0 +1,14 @@
+var div = document.getElementById("table-container");
+var canvas = document.getElementById("canvas");
+var input = document.getElementById("input-value");
+var ctx = canvas.getContext("2d");
+
+var cellHeight = 30;
+var cellWidth = 80;
+
+var firstRow = 0;
+var firstColumn = 0;
+
+const blue_accent_color = "#C3DBE3";
+const header_color = "#C3DBE3";
+const header_accent_color = "#99ADB4";
